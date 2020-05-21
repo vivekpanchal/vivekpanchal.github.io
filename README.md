7c8c895b5c1993df74927e9ccbac133b97dda919
vivek panchal <vivekpanchal64@gmail.com>
2020-05-21 15:51:44 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# vivekpanchal.github.io
---
+++ precache-manifest.18aed48afba9526cbd6e4fd19b39f5a6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d464ad085257878ccd63fe01c506ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "367019447d5166bafa17",
+    "url": "/static/css/main.73d081c3.chunk.css"
+  },
+  {
+    "revision": "236d396d74c851fc39bf",
+    "url": "/static/js/2.150d94a8.chunk.js"
+  },
+  {
+    "revision": "367019447d5166bafa17",
+    "url": "/static/js/main.ef1eb441.chunk.js"
+  },
+  {
+    "revision": "1b2017c8165bfcaf1260",
+    "url": "/static/js/runtime-main.3f59a11c.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  }
+]);
